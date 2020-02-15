5483d7e8ce2ead3b0a811a4f338a1cce204d260c
Josh Wulf <josh@magikcraft.io>
2020-02-15 13:26:06 +1000

    Allow workers access to zbc

--- src/workers.ts
+++ src/workers.ts
@@ -5,14 +5,14 @@ export async function bootstrapWorkers(
   workerCode: string,
   lifetime: number
 ): Promise<void> {
-  return new Promise((resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
     const __module: {
       exports?: {tasks?: {[key: string]: ZBWorkerTaskHandler}}
     } = {}
+    const zbc = new ZBClient()
     eval(`(function(module){${workerCode}})(__module)`)
     const tasks = __module.exports?.tasks
     if (tasks) {
-      const zbc = new ZBClient()
       for (const tasktype of Object.keys(tasks)) {
         core.info(`Starting worker for task type ${tasktype}...`)
         zbc.createWorker(null, tasktype, tasks[tasktype])
@@ -24,6 +24,7 @@ export async function bootstrapWorkers(
         resolve()
       }, lifetime * 60 * 1000)
     } else {
+      await zbc.close()
       reject(new Error(`No export 'tasks' found in handler file`))
     }
   })
